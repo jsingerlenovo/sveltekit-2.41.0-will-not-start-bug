90e6f9272cbc0d886711080e898037eb33e16700
Jason Singer <jsinger@lenovo.com>
2025-12-09 10:02:12 -0500

    Reproduction of the bug

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	let name = $state('world');
+	let count = $state(0);
+</script>
+
+<h1>Hello {name}!</h1>
+
+<input bind:value={name} />
+<button onclick={() => count += 1}>
+	clicks: {count}
+</button>
+
+<div>
+    <p>Notice the error in the Console tab of DevTools and notice how the button and the input bind doesn't work.</p>
+    <blockquote>
+        <p>The error should look like this:</p>
+        <pre>
+            Uncaught TypeError: Cannot read properties of undefined (reading 'start')
+                at (index):82:26
+        </pre>
+    </blockquote>
+</div>
